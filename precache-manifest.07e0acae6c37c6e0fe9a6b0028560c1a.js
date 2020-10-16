aa585ab5ff58a10f250c172e264b469ff0080b98
Vinhvp <vinh.hcmue.k42tina@gmail.com>
2020-10-16 21:09:12 +0700

    Updates

---
+++ precache-manifest.07e0acae6c37c6e0fe9a6b0028560c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2d30d4529e10187506cff7092dcde9",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "1ce11891531383e67b26",
+    "url": "/deploy-github/static/css/main.f8daa082.chunk.css"
+  },
+  {
+    "revision": "94ee9a2c984f779ab70e",
+    "url": "/deploy-github/static/js/2.5cc8c3de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deploy-github/static/js/2.5cc8c3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce11891531383e67b26",
+    "url": "/deploy-github/static/js/main.09bd8ffe.chunk.js"
+  },
+  {
+    "revision": "39317f873cd363a8eaa8",
+    "url": "/deploy-github/static/js/runtime-main.955354c0.js"
+  },
+  {
+    "revision": "fdb3e43885eaf1379ba49f84be456932",
+    "url": "/deploy-github/static/media/6U6A7926.fdb3e438.JPG"
+  }
+]);
